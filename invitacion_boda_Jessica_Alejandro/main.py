b1e1d7576c5b900ef0d69380cd1eebfc5a048a5d
AbrahamBarr <abrahamsar95@gmail.com>
2025-07-20 01:46:35 -0600

    Update main.py

--- invitacion_boda_Jessica_Alejandro/main.py
+++ invitacion_boda_Jessica_Alejandro/main.py
@@ -5,27 +5,15 @@ from fastapi.templating import Jinja2Templates
 
 import pandas as pd
 
-# Cargar el archivo Excel una sola vez al iniciar FastAPI
-excel_path = "C:\Users\a0s10rw\Desktop\Backend_boda\invitacion_boda_Jessica_Alejandro\data\invitados.xlsx" 
+excel_path = "data/invitados.xlsx"
 df = pd.read_excel(excel_path)
-
-# Filtrar filas con "Invitación dirigida a"
 df = df[df["Invitación dirigida a"].notna()]
-
-# Crear diccionario: { "Invitación dirigida a": Max Boletos }
 boletos_dict = df.groupby("Invitación dirigida a")["Max Boletos"].max().to_dict()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def invitacion(request: Request):
-    return templates.TemplateResponse("invitacion.html", {"request": request})
-
-@app.post("/confirmar")
-async def confirmar(nombre: str = Form(...), email: str = Form(...)):
-    return HTMLResponse(content=f"<h2>¡Gracias por confirmar, {nombre}!</h2>")
 
 @app.get("/", response_class=HTMLResponse)
 async def invitacion(request: Request):
@@ -33,3 +21,7 @@ async def invitacion(request: Request):
         "request": request,
         "boletos_dict": boletos_dict
     })
+
+@app.post("/confirmar")
+async def confirmar(nombre: str = Form(...), email: str = Form(...)):
+    return HTMLResponse(content=f"<h2>¡Gracias por confirmar, {nombre}!</h2>")
